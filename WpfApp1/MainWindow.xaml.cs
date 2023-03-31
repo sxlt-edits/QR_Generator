261fa8e19ddfdff7686ac7a4fecf6b56b505eddc
Timofey Raykin <Timofey Raykin@DESKTOP-4QIU603>
2023-03-31 23:06:29 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Drawing;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BT_QR_Click(object sender, RoutedEventArgs e)
+        {
+            QRCoder.QRCodeGenerator qrCodeGenerator = new QRCoder.QRCodeGenerator();
+            QRCoder.QRCodeData qRCodeData = qrCodeGenerator.CreateQrCode(TB_Text.Text, QRCoder.QRCodeGenerator.ECCLevel.L);
+            QRCoder.QRCode code = new QRCoder.QRCode(qRCodeData);
+            Bitmap bitmap = code.GetGraphic(100);
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+                QR_Image.Source = bitmapimage;
+            }
+        }
+
+    }
+}
